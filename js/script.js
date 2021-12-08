c07280af33b49a79c4c28a168f133feda83948f4
iamstillcath <catherine_lucia@yahoo.com>
2021-12-08 18:55:10 +0100

    color

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //Choose a random color
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue','black', 'yellow', 'pink', 'purple']
+const button = document.querySelector('#button')
+const body = document.querySelector('.body')
+const colors = ['red','orange','green', 'blue','black', 'yellow', 'pink', 'purple']
 
 body.style.backgroundColor = "silver"
 button.addEventListener('click', fridge)
